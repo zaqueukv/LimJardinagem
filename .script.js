9af38d331e09f25799eea03e292ec37520a4857f
Lim <161227168+zaqueukv@users.noreply.github.com>
2024-08-04 10:54:19 -0300

    Add files via upload

---
+++ .script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado pela sua mensagem! Entraremos em contacto em breve.');
+});
